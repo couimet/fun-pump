e85be950d684bb92868a2daab777aa0a303b7bdf
Charles Ouimet <charles.ouimet@gmail.com>
2025-01-17 13:38:07 -0500

    [01:01:24] Add `owner` to the `Factory` contract and test it

--- test/Factory.js
+++ test/Factory.js
@@ -6,18 +6,26 @@ describe("Factory", function () {
     const FEE = ethers.parseUnits("0.01", 18)
 
     async function deployFactoryFixture() {
+        // Fetch accounts
+        const [deployer] = await ethers.getSigners()
+
         // Fetch the contract
         const Factory = await ethers.getContractFactory("Factory")
         // Deploy the contract
         const factory = await Factory.deploy(FEE)
 
-        return { factory }
+        return { factory, deployer }
     }
 
     describe("Deployment", function () {
         it("Should set the fee", async function () {
             const { factory } = await loadFixture(deployFactoryFixture)
             expect(await factory.fee()).to.equal(FEE)
+        })
+
+        it("Should set the owner", async function () {
+            const { factory, deployer } = await loadFixture(deployFactoryFixture)
+            expect(await factory.owner()).to.equal(deployer.address)
         })
     })
 })
