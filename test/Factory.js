89965720bb349e4c5715872a6742d608a02fdeec
Charles Ouimet <charles.ouimet@gmail.com>
2025-01-17 09:46:20 -0500

    [00:43:16] Completed unit test on Factory (demo exercise for tests)

--- test/Factory.js
+++ test/Factory.js
@@ -4,4 +4,14 @@ const { ethers } = require("hardhat")
 
 describe("Factory", function () {
 
+    it("should have a name", async function() {
+        // Fetch the contract
+        const Factory = await ethers.getContractFactory("Factory")
+        // Deploy the contract
+        const factory = await Factory.deploy()
+        // Check name
+        const name = await factory.name()
+        // Check name is correct
+        expect(name).to.equal("Factory")
+    })
 })
