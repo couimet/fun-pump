a6fc5ac6b050380c01b25a61a82d456f05782274
Charles Ouimet <charles.ouimet@gmail.com>
2025-01-20 14:29:47 -0500

    [02:08:45] Add `Factory.getCost()` and leverage it in `Factory.buy()` to track the evolution of total of tokens bought

--- contracts/Factory.sol
+++ contracts/Factory.sol
@@ -32,6 +32,15 @@ contract Factory {
         return tokenToSale[tokens[_index]];
     }
 
+    function getCost(uint256 _sold) public pure returns(uint256) {
+        uint256 floor = 0.0001 ether;
+        uint256 step = 0.0001 ether;
+        uint256 increment = 10000 ether;
+
+        uint256 cost = (step * (_sold / increment)) + floor;
+        return cost;
+    }
+
     function create(
         string memory _name,
         string memory _symbol
@@ -67,8 +76,16 @@ contract Factory {
         TokenSale storage sale = tokenToSale[_token];
         // Check conditions
 
+        // Calculate the price of 1 token based upon total bought.
+        uint256 cost = getCost(sale.sold);
+
+        uint256 price = cost * (_amount / 10 ** 18);
+
+        // Make sure enough eth is sent
+
         // Update the sale
         sale.sold += _amount;
+        sale.raised += price;
 
         // Make sure fund raising goal isn't met
 
--- test/Factory.js
+++ test/Factory.js
@@ -120,6 +120,17 @@ describe("Factory", function () {
             const sale = await factory.tokenToSale(await token.getAddress())
 
             expect(sale.sold).to.equal(AMOUNT)
+            expect(sale.raised).to.equal(COST)
+            expect(sale.isOpen).to.equal(true)
+        })
+
+        it("Should increase base cost", async function () {
+            const { factory, token } = await loadFixture(buyTokenFixture)
+
+            const sale = await factory.tokenToSale(await token.getAddress())
+            const cost = await factory.getCost(sale.sold)
+
+            expect(cost).to.be.equal(ethers.parseUnits("0.0002"))
         })
     })
 })
