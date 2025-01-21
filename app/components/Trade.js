6e482ab43188f93cfd2b9edadfaec71c559c315b
Charles Ouimet <charles.ouimet@gmail.com>
2025-01-21 12:12:56 -0500

    [04:13:06] Add basic UI wiring for the upcoming _trade_ functionality

--- app/components/Trade.js
+++ app/components/Trade.js
@@ -5,7 +5,13 @@ function Trade({ toggleTrade, token, provider, factory }) {
 
   return (
     <div className="trade">
+      <h2>trade</h2>
 
+      <div className="token__details">
+        <p className="name">{token.name}</p>
+      </div>
+      
+      <button onClick={toggleTrade} className="btn--fancy">[ cancel ]</button>
     </div >
   );
 }
--- app/page.js
+++ app/page.js
@@ -20,10 +20,17 @@ export default function Home() {
   const [factory, setFactory] = useState(null)
   const [fee, setFee] = useState(0)
   const [tokens, setTokens] = useState([])
+  const [token, setToken] = useState(null)
   const [showCreate, setShowCreate] = useState(false)
+  const [showTrade, setShowTrade] = useState(false)
 
   function toggleCreate() {
     showCreate ? setShowCreate(false) : setShowCreate(true)
+  }
+
+  function toggleTrade(token) {
+    setToken(token)
+    showTrade ? setShowTrade(false) : setShowTrade(true)
   }
 
   async function loadBlockchainData() {
@@ -95,7 +102,7 @@ export default function Home() {
             ) : (
               tokens.map((token, index) => (
                 <Token
-                  toggleTrade={() => {}}
+                  toggleTrade={toggleTrade}
                   token={token}
                   key={index}
                 />
@@ -107,6 +114,10 @@ export default function Home() {
 
       {showCreate && (
         <List toggleCreate={toggleCreate} fee={fee} provider={provider} factory={factory} />
+      )}
+
+      {showTrade && (
+        <Trade toggleTrade={toggleTrade} token={token} provider={provider} factory={factory} />
       )}
     </div>
   );
