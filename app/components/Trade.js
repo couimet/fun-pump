fd6215adfa668d516c9a58ccd842cf1d3d76fa84
Charles Ouimet <charles.ouimet@gmail.com>
2025-01-21 12:17:03 -0500

    [04:14:09] Add more information to the `Trade` component

--- app/components/Trade.js
+++ app/components/Trade.js
@@ -9,8 +9,11 @@ function Trade({ toggleTrade, token, provider, factory }) {
 
       <div className="token__details">
         <p className="name">{token.name}</p>
+        <p>creator: {token.creator.slice(0, 6) + '...' + token.creator.slice(38, 42)}</p>
+        <img src={token.image} alt="token image" width={256} height={256} />
+        <p>marketcap: {ethers.formatUnits(token.raised, 18)} eth</p>
       </div>
-      
+
       <button onClick={toggleTrade} className="btn--fancy">[ cancel ]</button>
     </div >
   );
